f8257392b6eba47414b88d39c86553b4387ce93b
Alan Franco <alanfrancob97@gmail.com>
2025-02-03 16:14:42 -0600

    feat(editor): display connected users in editor

--- frontend/src/components/CollaborativeEditor.vue
+++ frontend/src/components/CollaborativeEditor.vue
@@ -6,12 +6,20 @@ import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
 import { EditorContent, useEditor } from '@tiptap/vue-3';
 
-import { onMounted } from 'vue';
+import { inject, ref } from 'vue';
 import * as Y from 'yjs';
 
 import config from '@/config/config';
 
+const keycloak = inject('keycloak');
 const doc = new Y.Doc();
+const provider = new TiptapCollabProvider({
+  baseUrl: config.HOCUSPOCUS_WS_BASE_URL,
+  name: 'example-document',
+  document: doc,
+});
+
+const connectedUsers = ref([keycloak.tokenParsed.name]);
 
 const editor = useEditor({
   extensions: [
@@ -24,24 +32,40 @@ const editor = useEditor({
   ],
 });
 
-onMounted(() => {
-  const provider = new TiptapCollabProvider({
-    baseUrl: config.HOCUSPOCUS_WS_BASE_URL,
-    name: 'example-document',
-    document: doc,
-    onSynced() {
-      if (!doc.getMap('config').get('initialContentLoaded') && editor) {
-        doc.getMap('config').set('initalContentLoaded', true);
-        editor.value.commands.setContent('Welcome! This is a locally-hosted Tiptap example! 🎉');
-      }
-    },
-  });
+provider.setAwarenessField('user', {
+  name: keycloak.tokenParsed.name,
+});
+
+provider.on('awarenessChange', ({ states }) => {
+  console.log(states);
+  connectedUsers.value = [];
+  states.forEach(state => connectedUsers.value.push(state.user.name));    
 });
 </script>
 
 <template>
-  <EditorContent :editor="editor" />
+  <EditorContent id="editor" :editor="editor" />
+  <hr />
+  <div id="connected-users-container">
+    <h3>Connected Users</h3>
+    <li v-for="user in connectedUsers">
+      {{ user }}
+    </li>
+  </div>
 </template>
 
 <style scoped>
+@reference '../main.css';
+
+h3 {
+  @apply text-xl
+}
+
+#connected-users-container {
+  @apply p-2
+}
+
+#editor {
+  @apply p-2
+}
 </style>
