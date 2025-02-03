0316ecf525e65e87d16e6b78682a70e61bc7d290
Alan Franco <alanfrancob97@gmail.com>
2025-02-03 14:21:48 -0600

    feat(navbar): add logout button to Navbar component

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,20 +1,38 @@
 <script setup>
+import { inject } from 'vue';
+
+const keycloak = inject('keycloak');
 </script>
 
 <template>
   <div id="navbar">
     <h1>Tiptap Example</h1>
+    <div id="button-container">
+      <button @click="keycloak.logout()">Logout</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
 @reference '../main.css';
 
+button {
+  @apply text-gray-200 text-xl p-1;
+}
+
+button:hover {
+  @apply cursor-pointer;
+}
+
 h1 {
   @apply text-gray-200 text-2xl font-bold;
 }
 
+#button-container {
+  @apply flex-1 text-right;
+}
+
 #navbar {
-  @apply p-2 bg-green-700;
+  @apply flex p-2 bg-green-700;
 }
 </style>
